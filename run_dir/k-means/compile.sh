07e66eb4e144931232cc305cde4da60ed735046c
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-12-05 21:37:06 +0900

    An EPS and a PDF can be generated.

--- run_dir/k-means/compile.sh
+++ run_dir/k-means/compile.sh
@@ -1,3 +1,9 @@
+# $ sh compile.sh
+# $ ./k-means ../../source2016/HokkaidoTowns_xy.dat 6 1
+# ^ 2nd arg: the number of clusters, 3rd arg: a seed of random values
+# $ gnuplot Hokkaido_xyl_gray.plt
+# $ ps2pdf Hokkaido_xyl_gray.eps
+
 g++ -std=c++11 -o k-means.exe \
  ../../source2016/k-means.cpp \
  ../../source2016/common.cpp

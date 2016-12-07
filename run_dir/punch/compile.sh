951d115ed152e25a79ea8e601e1833701f78ae20
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-12-07 12:06:46 +0900

    Comment has been updated.

--- run_dir/punch/compile.sh
+++ run_dir/punch/compile.sh
@@ -1,6 +1,7 @@
 # $ sh compile.sh
-# $ ./modPerceptron ../k-means/Hokkaido_xyl.dat 1 0.01
-# ^ 2nd arg: a seed of random values, 3rd arg: learning rate
+# $ ./genPunch ../../source2016/ratioFruits.dat \
+#   ../../source2016/priorPot.dat 50 50 1 > 50_50_1.dat
+# $ ./estParam 50_50_1_RatioFruits.dat 50_50_1_PriorPot.dat 50_50_1.dat 4 7
 
 g++ -std=c++11 -o genPunch.exe ../../source2016/genPunch.cpp
 g++ -std=c++11 -o estParam.exe ../../source2016/estParam.cpp
